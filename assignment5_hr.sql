3758f0089fa6fa1afebaf3bd7fa89a702894bc33
Sun ju Kim <ksunju0610@naver.com>
2019-12-15 23:06:31 +0900

    assignment : join(hr)

--- assignment5_hr.sql
+++ assignment5_hr.sql
@@ -1,5 +1,26 @@
--- hr
--- 문제1) EMPLOYEES 테이블과 DEPARTMENTS 테이블을 Cartesian
--- Product(모든 가능한 행들의 Join)하여 사원번호,이름,업무,부서번호,부서명,근무지를 출력하여라.
-select * from employees;
+--문제1) EMPLOYEES 테이블과 DEPARTMENTS 테이블을 
+--Cartesian Product(모든 가능한 행들의 Join)하여 사원번호,이름,업무,부서번호,부서명, 근무지를 출력하여라. 
+
+--문제2) EMPLOYEES 테이블에서 사원번호,이름,업무, EMPLOYEES 테이블의 부서번호, 
+--DEPARTMENTS 테이블의 부서번호,부서명,근무지를 출력하여라. 
+
+--문제3) Alexander Hunold 의 부서명을 출력하라. 
+
+--문제4) 세일즈 부서에서 근무하고 있는 사람들을 출력하라. 
+
+--문제5) EMPLOYEES 테이블과 DEPARTMENTS 테이블의 부서번호를 조인하고 
+--SA_MAN 사원만의 사원번호,이름,급여,부서명,근무지를 출력하라. (Alias를 사용) 
+
+--문제6) EMPLOYEES 테이블과 DEPARTMENTS 테이블에서 DEPARTMENTS 테이블에 있는 모든 자료를 
+--사원번호,이름,업무, EMPLOYEES 테이블의 부서번 호, 
+--DEPARTMENTS 테이블의 부서번호,부서명,근무지를 출력하여라 (Outer Join) 
+
+--문제7) EMPLOYEES 테이블에서 Self join하여 관리자(매니저)를 출력하여라. 
+
+--문제8) EMPLOYEES 테이블에서 left join하여 관리자(매니저)를 출력하고 
+--매니저 아이디가 없는 사람은 배제하고 하향식으로 하며, 급여는 역순으로 출력하라. 
+
+
+--문제9) EMPLOYEES 테이블에서 right join하여 
+--관리자(매니저)가 108번 상향식으로 급여는 역순으로 출력하라. 
 
